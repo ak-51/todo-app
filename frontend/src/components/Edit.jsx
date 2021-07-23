154740373349c487ae3e1a0b4661fea47abb747b
ak-51 <aditya.kumar.752cz@gmail.com>
2021-07-23 18:48:42 +0800

    fixed a bug

--- frontend/src/components/Edit.jsx
+++ frontend/src/components/Edit.jsx
@@ -1,12 +1,13 @@
 import axios from "axios"
 import { Fragment, useState } from "react"
 
-const Edit = ({desc, id}) => {
-    const [description, setDescription] = useState(desc)
+const Edit = ({todo}) => {
+    console.log(todo)
+    const [description, setDescription] = useState(todo.description)
 
     const editTodo = () => {
         try {
-            axios.put(`/todos/${id}`, {
+            axios.put(`/todos/${todo.todo_id}`, {
                 description: description
             })
             .then(res => {
@@ -19,24 +20,24 @@ const Edit = ({desc, id}) => {
     }
     return(
         <Fragment>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target={`#id${todo.todo_id}`}>
                 Edit
             </button>
 
 
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Edit Todo</h5>
+            <div className="modal fade" id={`id${todo.todo_id}`} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Edit Todo</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="modal-body">
+                        <div className="modal-body">
                             <input type="text" className="form-control" value={description} onChange={(e) => setDescription(e.target.value)} />
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary" onClick={editTodo}>Save changes</button>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary" onClick={editTodo}>Save changes</button>
                         </div>
                     </div>
                 </div>
--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -47,7 +47,7 @@ const List = () => {
                     {todos.map(todo => (
                         <tr key={todo.todo_id}>
                             <td>{todo.description}</td>
-                            <td><Edit desc={todo.description} id={todo.todo_id} /></td>
+                            <td><Edit todo={todo} /></td>
                             <td><button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
                         </tr>
                     ))}
